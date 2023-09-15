8a55baac8bdb39ce6b4f337e225f7ac07c3951f2
KSenamela <klaassenamela@gmail.com>
2023-09-15 19:33:04 +0200

    Added a Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+
+import Demo from './components/Demo';
+import Hero from './components/Hero';
+
+import './App.css';
+
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <main>
+      <div className='main'>
+        {/* Applying a gradient background to main page */}
+        <div className='gradient' /> 
+      </div>
+
+      <div className='app'>
+            <Hero />
+            <Demo />
+      </div>
+    </main>
   )
 }
 
